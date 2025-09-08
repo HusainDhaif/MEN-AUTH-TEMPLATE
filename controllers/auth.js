2244bfa99a11446dba07eb6db71600ec89bda20e
Husain Dhaif <hussaindhaif10@gmail.com>
2025-09-08 20:11:07 +0300

    registration complete

---
+++ controllers/auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user.js");
+
+
+router.get("/sign-up", (req, res) => {
+  res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+  res.send("Form submission accepted!");
+  
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+  return res.send("Username already taken.");
+}
+
+if (req.body.password !== req.body.confirmPassword) {
+  return res.send("Password and Confirm Password must match");
+}
+const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+req.body.password = hashedPassword;
+
+
+// validation logic
+
+const user = await User.create(req.body);
+res.send(`Thanks for signing up ${user.username}`);
+
+res.send(req.body);
+});
+
+
+module.exports = router;
